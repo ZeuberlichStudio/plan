5b80595b913a50185d4d21ecbac48177e12abaf8
Yaroslav <goooglemaaan@gmail.com>
2020-07-14 14:53:39 +0300

    Updates

---
+++ precache-manifest.36b2660b88103ceee8d14806d4c7a7d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4969860b75641109dbd7014f4821128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "992f69f19efb6009737d",
+    "url": "/static/css/main.c983ae1c.chunk.css"
+  },
+  {
+    "revision": "61ba79967f7297dad0a3",
+    "url": "/static/js/2.71312e93.chunk.js"
+  },
+  {
+    "revision": "c41e8a8d9ddc23896a1cf98f6e21e916",
+    "url": "/static/js/2.71312e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992f69f19efb6009737d",
+    "url": "/static/js/main.42b3e700.chunk.js"
+  },
+  {
+    "revision": "56be8004f5f2aaa7688a",
+    "url": "/static/js/runtime-main.e821da00.js"
+  },
+  {
+    "revision": "47ec6dcb95a360ac5e7b6900431432bb",
+    "url": "/static/media/mapIcon.47ec6dcb.svg"
+  }
+]);
