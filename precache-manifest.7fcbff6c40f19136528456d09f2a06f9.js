bda8cd9eeb2f9dc7e1a2c6e32b1360d865c7ead8
Yaroslav <goooglemaaan@gmail.com>
2020-08-06 17:11:05 +0300

    Updates

---
+++ precache-manifest.7fcbff6c40f19136528456d09f2a06f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe91d08f8eee111ffce6de13c1d0450",
+    "url": "/plan/index.html"
+  },
+  {
+    "revision": "043b688164afa0c33528",
+    "url": "/plan/static/css/main.c7eeb0ef.chunk.css"
+  },
+  {
+    "revision": "61ba79967f7297dad0a3",
+    "url": "/plan/static/js/2.71312e93.chunk.js"
+  },
+  {
+    "revision": "c41e8a8d9ddc23896a1cf98f6e21e916",
+    "url": "/plan/static/js/2.71312e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043b688164afa0c33528",
+    "url": "/plan/static/js/main.8eb94072.chunk.js"
+  },
+  {
+    "revision": "aae21ba712d523791323",
+    "url": "/plan/static/js/runtime-main.34f20e5f.js"
+  },
+  {
+    "revision": "47ec6dcb95a360ac5e7b6900431432bb",
+    "url": "/plan/static/media/mapIcon.47ec6dcb.svg"
+  }
+]);
